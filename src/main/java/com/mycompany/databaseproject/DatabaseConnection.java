643befc2f945e44c178d061168523bbc54ad7b14
AsmaKhourdaji <asmakhourdaji@gmail.com>
2025-12-05 14:26:35 +0300

    edit

---
+++ src/main/java/com/mycompany/databaseproject/DatabaseConnection.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.databaseproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author hadalkharouf
+ */
+public class DatabaseConnection {
+    
+
+    public static Connection getConnection() {
+        Connection conn = null;
+
+        try {
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=FixedAssestHirePurchaseDB;encrypt=false;";
+            String user = "LoginAsma";
+            String password = "12345";
+
+
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected Successfully!");
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return conn;
+}
+}
